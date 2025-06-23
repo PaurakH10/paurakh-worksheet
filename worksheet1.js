5d6286a0026e792321791670bee1bae274b33353
PaurakH10 <paurakhbhandari847@gmail.com>
2025-06-23 13:42:12 +0545

    Update worksheet1.js
    dv

--- worksheet1.js
+++ worksheet1.js
@@ -1,16 +1,18 @@
-//Worksheet 1: Variables and Data Types
-
-// This is a simple JavaScript program that declares three variables
-// and prints their values to the console.
-let name = "Paurakh";
-let age = 17;
-let student = true;
-
-console.log(name);
-console.log(age);
-console.log(student);
-/*output:
-Paurakh
-17
-true
-*/
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        let name = "Paurakh";
+        let age = 17;
+        let Student = false;
+        console.log(name);
+        console.log(age);
+        console.log(Student);
+    </script>
+</body>
+</html>
